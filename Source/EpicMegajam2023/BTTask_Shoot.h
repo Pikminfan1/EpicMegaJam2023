07f208044767ce2eed093ef8d248b8d88cd81385
Ryan Rose <ryanprose96@gmail.com>
2023-09-21 16:20:08 -0400

    Made Basic AI Behavior

---
+++ Source/EpicMegajam2023/BTTask_Shoot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_Shoot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICMEGAJAM2023_API UBTTask_Shoot : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
